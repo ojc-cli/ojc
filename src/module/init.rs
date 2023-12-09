5133ec939bc5a19db5ea6a37cb51950a56eb9d38
JingZi Fang <smallfang@rotriw.tech>
2023-12-09 22:30:16 +0800

    init: add folder init

--- src/module/init.rs
+++ src/module/init.rs
@@ -1,3 +1,6 @@
+use std::fs;
+use std::path::Path;
+
 use clap::Command;
 use crate::basics::basic;
 use crate::basics;
@@ -8,9 +11,19 @@ pub fn command() -> Command {
     .arg_required_else_help(false)
 }
 
+pub fn init_folder(path: &String) {
+    println!("Init the folder");
+    // println!("{:?}", );
+    let parser = String::from(shellexpand::tilde(path));
+    fs::create_dir_all(Path::new(&parser)).unwrap();
+    fs::create_dir_all(Path::new(&parser).join("oj")).unwrap();
+}
+
 pub fn matchd(_cli :&clap::ArgMatches) {
     println!("Init the cli");
     let mut config = json::JsonValue::Object(json::object::Object::new());
-    config["storage"] = json::JsonValue::String(basic::ask_storage_path());
+    let config_path = basic::ask_storage_path();
+    config["storage"] = json::JsonValue::String(config_path.clone());
     basics::config::set_config("config.json", config);
+    init_folder(&config_path);
 }
